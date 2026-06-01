8b35a9832f4da68ca613d1b35a05dde66993f38e
sakura35p <155404331+sakura35p@users.noreply.github.com>
2026-06-01 14:46:22 +0000

    navbar practice

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,35 @@ import type { Component } from 'solid-js';
 
 const App: Component = () => {
   return (
-    <p 
-    min-h-100vh
-    flex
-    items-center
-    justify-center
-    text="2xl blue-500">
-      Hello World!
-    </p>
+    <div 
+    min-h-screen
+    border="5px solid red">
+      <div
+      h-5vh
+      flex
+      items-center
+      border="1px solid blue">
+        <p 
+        border="1px solid green">
+          study 
+        </p>
+        <div
+        ml-auto
+        flex
+        gap-8
+        border="1px solid"
+        >
+        <p
+        border="1px solid green">
+          somthing...
+        </p>
+        <p
+        border="1px solid purple">
+          login
+        </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
